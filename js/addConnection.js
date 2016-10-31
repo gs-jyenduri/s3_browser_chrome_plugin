6c6ca4497bc8e519b53d0efcc86f12d8a71d4944
= <=>
2016-10-31 15:25:28 +0530

    [WIP]BucketName in BreadCrub fixed

--- js/addConnection.js
+++ js/addConnection.js
@@ -4,16 +4,17 @@ var common= '<li class="breadcrumb-item"> Amazon S3:</li>'
 
 /**
  *  This triggers on click of s3Connection
+ *
  */
 function  addConnection(){
-      $("#connectionName").val("");
-      $("#accessKey").val("");
-      $("#secretKey").val("");
-       $("#bucketName").val("");
-       $("#bucketConfiguration").removeClass("hide");
-       $("#bucketFooter").removeClass("hide");
-       $(".populateConnections").addClass("hide");
-       $("#testresult").addClass("hide");
+    $("#connectionName").val("");
+    $("#accessKey").val("");
+    $("#secretKey").val("");
+    $("#bucketName").val("");
+    $("#bucketConfiguration").removeClass("hide");
+    $("#bucketFooter").removeClass("hide");
+    $(".populateConnections").addClass("hide");
+    $("#testresult").addClass("hide");
 }
 /**
  * To hide the creationPanel
@@ -50,7 +51,7 @@ function populateConnections(){
     var columns="";
     chrome.storage.sync.get(null, function(items) {
         $("#connectionTables").empty();
-         allKeys = Object.keys(items);
+        allKeys = Object.keys(items);
         allKeys.forEach(function(key){
             columns=columns+"<tr> + <td data-id="+key+"> "+key+" </td><td><button class='btn btn-success btn-sm' data-connect="+key+" type='button'> Connect</button> </td><td><button class='btn btn-danger btn-sm'  id='deleteRow' data-delete="+key+" type='button'> Delete</button> </td> </tr>";
         });
@@ -102,27 +103,31 @@ function showModal(){
     $('#myModal').modal('show');
 }
 
-function connectionView(evt){
+function connectionView($evt){
+    $evt.stopPropagation();
+    var s3BucketName = $evt.target.getAttribute('data-connect');
+    this.name1= s3BucketName;
+    var self= this;
     $(".addconnection").addClass("hide");
     $(".viewconnection").removeClass("hide");
     $('#myModal').modal('hide');
     $("#prefix").empty();
     $("#prefix").append(common);
-    chrome.storage.sync.get($(evt.target).attr("data-connect"), function (obj) {
-        $("#prefix").append('<li class="breadcrumb-item"> <a href="#" data-root="'+obj.bucketName+'"'+ obj.bucketName+'</li>');
-        localStorage.setItem("accessKey",obj.accessKey);
-        localStorage.setItem("secretToken",obj.secretToken);
-        localStorage.setItem("bucketName",obj.bucketName);
+    chrome.storage.sync.get(s3BucketName, function (obj) {
+        $("#prefix").append('<li class="breadcrumb-item"> <a href="#" data-root="'+obj[self.name1].bucketName+'">'+ obj[self.name1].bucketName+'</a></li>');
+        localStorage.setItem("accessKey",obj[self.name1].accessKey);
+        localStorage.setItem("secretToken",obj[self.name1].secretToken);
+        localStorage.setItem("bucketName",obj[self.name1].bucketName);
         var param= {"prefix":'', "delimiter":'/'};
         requestS3(localStorage.getItem("bucketName"),localStorage.getItem("accessKey"),localStorage.getItem("secretToken"),param,"GET","xml",cb_success);
         var cb_success= function(data){
             $("#dataTable").empty();
-           var rows="";
+            var rows="";
             var prefixes=xml2json(data,"");
             prefixes.forEach(function(prefix){
                 rows=rows+'<tr><td><a href="#" data-prefix="'+prefix+'">'+prefix+'</a></td></tr>'
             });
-          $("#dataTable").append(rows);
+            $("#dataTable").append(rows);
         }
     });
 }
